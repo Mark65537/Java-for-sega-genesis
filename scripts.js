ad26fbf8ddf11ab7ca9b64dad73af934e7349299
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2024-07-03 22:29:08 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+function loadPage(language) {
+    const content = document.getElementById('content');
+    fetch(`${language}.html`)
+        .then(response => response.text())
+        .then(data => {
+            content.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading page:', error);
+        });
+}
